641f2f0d366fd910c05e2abedcba539a02d9049a
Ma5terLink <devalon@yandex.ru>
2021-08-22 22:39:47 +0500

    07 - Верстка завершена. The layout is completed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 // Плавный скролл и класс pageup
+const pageCH = document.querySelector('.pageup');
+
 $(window).scroll(function () {
+    console.log($(this).scrollTop());
     if ($(this).scrollTop() > 700) {
         $('.pageup').fadeIn();
     } else {
         $('.pageup').fadeOut();
+    }
+    if ($(this).scrollTop() > 2080) {
+        pageCH.classList.add('change');
+    } else {
+        pageCH.classList.remove('change');
     }
 });
 
